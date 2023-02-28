059dc2dc7d269b6b758d4dabd4b1416e1c16b4ca
eldhosevj <eldhosvj@gmail.com>
2023-02-28 10:51:47 +0530

    removed unwanted folder

--- Documents/src/App.js
+++ Documents/src/App.js
@@ -1,27 +1,12 @@
-import React ,{useState}from "react";
-import { Route, Switch, } from "react-router-dom";
+import React from "react";
 import "./App.scss";
-import '@progress/kendo-theme-default/dist/all.css';
-import './index.css';
-import './App.css'
-
-import DocumentPage from "./components/Pages/DocumentPage";
+import View from "./components/Document/View";
 
 const App = () => {
-  const [route, setRoute] = useState("null");
-  const navigationClicked = (url) => {
-    if (url == "/") {
-      localStorage.setItem("url", url);
-      setRoute(url);
-    }
-  }
   return (
-    <>
-     <div className="App">
-      <DocumentPage></DocumentPage>
+    <div className="App">
+      <View />
     </div>
-    </>
-   
   );
 };
 
--- Documents/src/components/Documents/Document.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Document = () => {
-  return (
-    <div className="wrapper">
-      <div className="main_content">Welcome to Documentpage</div>
-    </div>
-  );
-};
-export default Document;
