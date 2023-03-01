4bcc29ff176d056b903127befc639d18fcf7bae9
shubham2010-tech <sp638733@gmail.com>
2023-03-01 09:45:36 +0530

    Error text and color changes

--- Container/src/Components/SelectRoute.js
+++ Container/src/Components/SelectRoute.js
@@ -5,14 +5,14 @@ const Logs = React.lazy(() => import("Vtrac/Logs"));
 const View = React.lazy(() => import("Documents/View"));
 const Customer = React.lazy(() => import("SharedData/Customer"));
 const RecievedShipment = React.lazy(() => import("Shipping/RecievedShipment"));
-const ViewShipment = React.lazy(() => import("Shipping/ViewShipment"))
-import ErrorBoundaryVtrackOrders from "../error/Vtrack/ErrorBoundaryVtrackOrders"
-import ErrorBoundaryVtrackLogs from "../error/Vtrack/ErrorBoundaryVtrackLogs"
-import ErrorBoundaryVtrackReports from "../error/Vtrack/ErrorBoundaryVtrackReports"
-import ErrorBoundaryCustomers from "../error/SharedData/ErrorBoundaryCustomers"
-import ErrorBoundaryViewShipment from "../error/Shipping/ErrorBoundaryViewShipment"
-import ErrorBoundaryRecievedShipment from "../error/Shipping/ErrorBoundaryRecievedShipment"
-import ErrorBoundaryDocumentView from "../error/Document/ErrorBoundaryDocumentView"
+const ViewShipment = React.lazy(() => import("Shipping/ViewShipment"));
+import ErrorBoundaryVtrackOrders from "../error/Vtrack/ErrorBoundaryVtrackOrders";
+import ErrorBoundaryVtrackLogs from "../error/Vtrack/ErrorBoundaryVtrackLogs";
+import ErrorBoundaryVtrackReports from "../error/Vtrack/ErrorBoundaryVtrackReports";
+import ErrorBoundaryCustomers from "../error/SharedData/ErrorBoundaryCustomers";
+import ErrorBoundaryViewShipment from "../error/Shipping/ErrorBoundaryViewShipment";
+import ErrorBoundaryRecievedShipment from "../error/Shipping/ErrorBoundaryRecievedShipment";
+import ErrorBoundaryDocumentView from "../error/Document/ErrorBoundaryDocumentView";
 import ErrorBoundary from "../error/ErrorBoundary";
 const SelectRoute = (props) => {
   const url = props.url;
@@ -20,9 +20,16 @@ const SelectRoute = (props) => {
     if (url == "/Orders") {
       return (
         <Suspense fallback={null}>
-           <ErrorBoundaryVtrackOrders  fallback={<h2>Unable to load Orders</h2>}>
-
-          <Orders />
+          <ErrorBoundaryVtrackOrders
+            fallback={
+              <h2>
+                V-Trac server is down.
+                <br />
+                Sorry for the inconvenience caused.
+              </h2>
+            }
+          >
+            <Orders />
           </ErrorBoundaryVtrackOrders>
         </Suspense>
       );
@@ -30,8 +37,16 @@ const SelectRoute = (props) => {
     if (url == "/Reports") {
       return (
         <Suspense fallback={null}>
-           <ErrorBoundaryVtrackReports  fallback={<h2>Unable to load Reports</h2>}>
-          <Reports />
+          <ErrorBoundaryVtrackReports
+            fallback={
+              <h2>
+                V-Trac server is down.
+                <br />
+                Sorry for the inconvenience caused.
+              </h2>
+            }
+          >
+            <Reports />
           </ErrorBoundaryVtrackReports>
         </Suspense>
       );
@@ -39,18 +54,32 @@ const SelectRoute = (props) => {
     if (url == "/Logs") {
       return (
         <Suspense fallback={null}>
-           <ErrorBoundaryVtrackLogs  fallback={<h2>Unable to load Logs</h2>}>
-          <Logs />
+          <ErrorBoundaryVtrackLogs
+            fallback={
+              <h2>
+                V-Trac server is down.
+                <br />
+                Sorry for the inconvenience caused.
+              </h2>
+            }
+          >
+            <Logs />
           </ErrorBoundaryVtrackLogs>
         </Suspense>
-       
       );
     }
     if (url == "/Customer") {
       return (
         <Suspense fallback={null}>
-           <ErrorBoundaryCustomers  fallback={<h2>Unable to load Customer</h2>}>
-          <Customer />
+          <ErrorBoundaryCustomers
+            fallback={
+              <h2>
+                SharedData page is down. <br /> Sorry for the inconvenience
+                caused.
+              </h2>
+            }
+          >
+            <Customer />
           </ErrorBoundaryCustomers>
         </Suspense>
       );
@@ -58,8 +87,15 @@ const SelectRoute = (props) => {
     if (url == "/RecievedShipment") {
       return (
         <Suspense fallback={null}>
-           <ErrorBoundaryRecievedShipment  fallback={<h2>Unable to load RecievedShipment</h2>}>
-          <RecievedShipment />
+          <ErrorBoundaryRecievedShipment
+            fallback={
+              <h2>
+                Shipping Page is down. <br /> Sorry for the inconvenience
+                caused.
+              </h2>
+            }
+          >
+            <RecievedShipment />
           </ErrorBoundaryRecievedShipment>
         </Suspense>
       );
@@ -67,31 +103,37 @@ const SelectRoute = (props) => {
     if (url == "/ViewShipment") {
       return (
         <Suspense fallback={null}>
-        <ErrorBoundaryViewShipment  fallback={<h2>Unable to load view shipment</h2>}>
-
-          <ViewShipment />
+          <ErrorBoundaryViewShipment
+            fallback={
+              <h2>
+                Shipping Page is down. <br /> Sorry for the inconvenience
+                caused.
+              </h2>
+            }
+          >
+            <ViewShipment />
           </ErrorBoundaryViewShipment>
-
         </Suspense>
       );
     }
     if (url == "/View") {
       return (
         <Suspense fallback={null}>
-           <ErrorBoundaryDocumentView  fallback={<h2>Unable to load Documents</h2>}>
-          <View />
+          <ErrorBoundaryDocumentView
+            fallback={
+              <h2>
+                Document page is down. <br /> Sorry for the inconvenience
+                caused.
+              </h2>
+            }
+          >
+            <View />
           </ErrorBoundaryDocumentView>
         </Suspense>
       );
     }
   };
 
-
-  return( <div>
-  
-  {component()}
- 
-
-  </div>)
+  return <div>{component()}</div>;
 };
 export default SelectRoute;
