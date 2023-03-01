90458e5e13460f3a6b44682c35f681465c60b928
eldhosevj <eldhosvj@gmail.com>
2023-03-01 19:37:00 +0530

    axios

--- V-Trac/src/components/Orders/Orders.js
+++ V-Trac/src/components/Orders/Orders.js
@@ -1,8 +1,23 @@
 import React from "react";
+import axios from 'axios';
 const Orders = () => {
+  const getHeader=()=>{
+    axios.get('URL')
+  .then( (response)=> {
+    // handle success
+    console.log(response,"sucees");
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error,"success");
+  })
+  }
   return (
     <div className="wrapper">
-      <div className="main_content">Welcome to Orders</div>
+      <div className="main_content"><h6>Welcome to Orders</h6>
+      <button onClick={()=>{getHeader()}}>Token</button>
+      </div>
+      
     </div>
   );
 };
